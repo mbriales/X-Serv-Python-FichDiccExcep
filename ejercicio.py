d581a15ec04a0f9b7bbd19b2c0f88886f87c350f
Miguel Briales Romanillos <brialesromanillos@gmail.com>
2016-02-10 20:48:33 +0100

    Upload Ejercicio

--- ejercicio.py
+++ ejercicio.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# Miguel Briales Romanillos
 
-# hola, hola
+# Opens and reads file
+File_Users = open("/etc/passwd")  # No tiene "modo" -> Por defecto es "Read"
+User_Names = File_Users.readlines()
 
-fd = open('/etc/passwd', 'r')
+# Searches user names
+User_List = {}
+for Users in User_Names:
+    if Users.find('#'):   # For delete comments
+        # Looks for user
+        End_User = Users.find(':*:')
+        User = Users[:End_User]
+        # Looks for shell
+        Users_Reverted = Users[::-1]
+        Start_Shell = Users_Reverted.find(':')
+        Shell = Users_Reverted[:Start_Shell]
+        Shell = Shell[::-1]
 
-lineas = fd.readlines()
-fd.close()
+# Writes the dictionary
+        User_List[User] = Shell
 
-for linea in lineas:
-    elementos = linea.split(':')
-    print elementos[0], elementos[-1][:-1]
+# Prints number of users
+print ('Hay ' + str(len(User_List)) + ' usuarios\n')
+
+# Prints "root" and "imaginary" user
+try:
+    print 'Usuario root: '
+    print User_List['root']
+    print 'Usuario imaginario %d:' % User_List['imaginario']
+# In case an user doesn't exist
+except:
+    print 'ERROR! El usuario introducido no existe\n'
